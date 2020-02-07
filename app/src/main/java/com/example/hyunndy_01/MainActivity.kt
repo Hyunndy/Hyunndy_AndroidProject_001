a3be3dfd1c27117e610adfa47500d91229c5c0a3
user <yoosa3004@naver.com>
2020-02-07 16:44:30 +0900

    for Studying Activity Lifecycle

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -2,14 +2,48 @@ package com.example.hyunndy_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("abcd", "On Create");
+
     }
-    //랄라울랄라
-    //Test
-    //testy2
+    override fun  onStart()
+    {
+        super.onStart();
+        Log.d("abcd", "on start");
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("abcd", "on restart");
+    }
+
+    override fun onResume()
+    {
+        super.onResume();
+        Log.d("abcd", "on Resume");
+    }
+
+    override fun onPause() {
+        super.onPause();
+        Log.d("abcd", "On Pause");
+    }
+
+    override fun onStop()
+    {
+        super.onStop();
+        Log.d("abcd", "On Stop");
+    }
+
+    override fun onDestroy()
+    {
+        super.onDestroy();
+        Log.d("abcd","On Destroy");
+    }
 }
