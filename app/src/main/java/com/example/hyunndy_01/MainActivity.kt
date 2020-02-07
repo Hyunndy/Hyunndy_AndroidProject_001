8ff67413e80c71695b57c43dd0d37c07d7391edc
user <yoosa3004@naver.com>
2020-02-07 23:47:44 +0900

    for Studying ProgressBar

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -16,86 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        /*
         button.setOnClickListener { view ->
-
-            when (RadioGroup.checkedRadioButtonId) {
-                R.id.radioButton ->
-                    textView.text = "눈";
-                R.id.radioButton2 ->
-                    textView.text = "코";
-                R.id.radioButton3 ->
-                    textView.text = "입";
-            }
-
-            when (RadioGroup2.checkedRadioButtonId) {
-                R.id.radioButton4 ->
-                    textView2.text = "팔";
-                R.id.radioButton5 ->
-                    textView2.text = "다리";
-                R.id.radioButton6 ->
-                    textView2.text = "허리";
-            }
-        }
-        */
-
-
-       // var Listener2 = RadioListener();
-       // RadioGroup.setOnCheckedChangeListener(Listener2);
-       // RadioGroup2.setOnCheckedChangeListener(Listener2);
-
-        RadioGroup.setOnCheckedChangeListener { group, checkedId ->
-            when(checkedId)
-            {
-                R.id.radioButton->
-                    textView.text = "체크 이벤트 : 눈";
-                R.id.radioButton2->
-                    textView.text = "체크 이벤트 : 코";
-                R.id.radioButton3->
-                    textView.text = "체크 이벤트 : 입";
-            }
-        }
-
-        RadioGroup2.setOnCheckedChangeListener { group, checkedId ->
-            when(checkedId)
-            {
-                R.id.radioButton->
-                    textView2.text = "체크 이벤트 : 눈";
-                R.id.radioButton2->
-                    textView2.text = "체크 이벤트 : 코";
-                R.id.radioButton3->
-                    textView2.text = "체크 이벤트 : 입";
-            }
-        }
-
-    }
-
-    inner class RadioListener:RadioGroup.OnCheckedChangeListener
-    {
-        override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-            when(group?.id)
-            {
-                R.id.RadioGroup ->
-                    when(checkedId)
-                    {
-                        R.id.radioButton->
-                            textView.text = "체크 이벤트 : 눈";
-                        R.id.radioButton2->
-                            textView.text = "체크 이벤트 : 코";
-                        R.id.radioButton3->
-                            textView.text = "체크 이벤트 : 입";
-                    }
-                R.id.RadioGroup2 ->
-                    when(checkedId) {
-                        R.id.radioButton4 ->
-                            textView2.text = "체크 이벤트 : 허리";
-                        R.id.radioButton5 ->
-                            textView2.text = "체크 이벤트 : 다리";
-                        R.id.radioButton6 ->
-                            textView2.text = "체크 이벤트 : 팔";
-                    }
-            }
+            progressBar.incrementProgressBy(5);
+            progressBar2.incrementProgressBy(5);
+            progressBar3.incrementProgressBy(5);
+            progressBar4.incrementProgressBy(5);
         }
     }
 }
