15d9da674f19d7b1400827295041862f81f7cef5
user <yoosa3004@naver.com>
2020-02-07 21:40:34 +0900

    for Studying CheckBox

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import android.util.Log
 import android.view.View
+import android.widget.CompoundButton
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 
@@ -14,69 +15,75 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 리스너 객체 생성
-        var listner = BtnListner();
-        button16.setOnClickListener(listner);
+        button.setOnClickListener { view ->
+            textView6.text = " ";
 
-        var listner2 = BtnListner2();
-        button2.setOnClickListener(listner2);
-
-        var listner3 = BtnListner3();
-        button3.setOnClickListener(listner3);
-        button4.setOnClickListener(listner3);
-
-
-
-        //람다식으로 한다면?
-        button5.setOnClickListener { view ->
-            textView1.text = "이춘덕";
-        }
-
-        button6.setOnClickListener { view->
-            textView1.text = "8월8일";
-        }
-
-        var listener4 = View.OnClickListener { view ->
-            when(view.id) {
-                R.id.button7 ->
-                    textView1.text = "여자";
-                R.id.button8 ->
-                    textView1.text = "아톰";
-            }
-        }
-
-        button6.setOnClickListener(listener4);
-        button7.setOnClickListener(listener4);
-    }
-
-    // 자바 인터페이스는 implements
-    // 코틀린에서는 상속이나 인터페이스 구현할 때 그냥 :콜론을 붙인다.
-    inner class BtnListner: View.OnClickListener//인터페이스
-    {
-        override fun onClick(v: View?) {
-            textView1.text = "지존 현지 지존 짱";
-        }
-    }
-
-    inner class BtnListner2:View.OnClickListener
-    {
-        override fun onClick(v: View?) {
-            textView1.text = "존예 현지 존나 예뻐";
-        }
-    }
-
-    inner class BtnListner3:View.OnClickListener
-    {
-        override fun onClick(v: View?) {
-            when(v?.id)
+            if(checkBox.isChecked)
             {
-                R.id.button3 ->
-                    textView1.text = "존못";
-                R.id.button4 ->
-                    textView1.text = "2월7일";
+                //문자열 뒤에 추가적으로 붙게 되는것.
+                textView6.append("현지는 존예이다\n");
             }
-
+            if(checkBox2.isChecked)
+            {
+                textView6.append("현지는 지존이다\n");
+            }
+            if(checkBox3.isChecked)
+            {
+                textView6.append("거짓입니다.\n");
+            }
         }
+
+
+        button2.setOnClickListener { view ->
+            checkBox.isChecked = true;
+            checkBox2.isChecked = true;
+            checkBox3.isChecked = true;
+        }
+
+        button3.setOnClickListener { view ->
+            checkBox.toggle();
+            checkBox2.toggle();
+            checkBox3.toggle();
+        }
+
+        var listener2 = CheckBoxListener();
+        checkBox.setOnCheckedChangeListener(listener2);
+
+        checkBox2.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked)
+            {
+                textView6.text = ("이벤트 : 체크박스 2가 체크되었습니다.");
+            }
+            else
+            {
+                textView6.text = ("이벤트 : 체크박스 2가 체크되지않았습니다.");
+            }
+        }
+
+        checkBox3.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked)
+            {
+                textView6.text = ("이벤트 : 체크박스 3가 체크되었습니다.");
+            }
+            else
+            {
+                textView6.text = ("이벤트 : 체크박스 3가 체크되지않았습니다.");
+            }
+        }
+
     }
 
+    inner class CheckBoxListener:CompoundButton.OnCheckedChangeListener
+    {
+        override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+            if(isChecked)
+            {
+                textView6.text = "이벤트 : 체크박스 1이 체크되었다.";
+            }
+            else
+            {
+                textView6.text = "이벤트 : 체크박스 1이 해제되었다.";
+            }
+        }
+    }
 }
