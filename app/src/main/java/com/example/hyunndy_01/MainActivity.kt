c78a36e44eec692b5e4429fb14d4fc230de1339d
user <yoosa3004@naver.com>
2020-02-08 21:27:18 +0900

    for Studying EditText

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.hyunndy_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
-import android.widget.CompoundButton
-import android.widget.RadioGroup
-import android.widget.SeekBar
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
 
@@ -18,50 +18,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        button.setOnClickListener { view ->
+            textView.text = editText.text;
+        }
+
+        button2.setOnClickListener { view ->
+            editText.setText("지존현지");
+        }
 
 
-        //오버라이딩 할 함수가 1개라면 람다식으로 하는게 좋지만,
-        seekBar.setOnSeekBarChangeListener( view ->
+       // var listener1 = EnterListener();
+       // editText.setOnEditorActionListener(listener1);
 
-        )
+        editText.setOnEditorActionListener { v, actionId, event ->
+            textView.text = editText.text;
+            false
+        }
 
+        var watch = Watcher();
+        editText.addTextChangedListener(watch);
 
-        //오버라이딩 할 함수가 2개 이상이라면 익명 중첩 클래스를 이용하는게 좋다.
-        seekBar2.setOnSeekBarChangeListener( object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        editText.addTextChangedListener( object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                textView.text = s;
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            override fun afterTextChanged(s: Editable?) {
 
             }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-            }
-
         })
-
-        var listener2 = SeekListener();
-        seekBar.setOnSeekBarChangeListener(listner2);
     }
 
-    inner class SeekListener:SeekBar.OnSeekBarChangeListener
-    {
-
-        // SeekBar 값이 변경되었을 때(코드로 바꾼경우, 유저에 의해 바뀐 경우)
-        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+    inner class Watcher : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
 
         }
 
-        // 값을 변경하기 위해 사용자가 터치했을 때
-        override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
         }
 
-        // 값을 변경한 후 터치를 때었을 때
-        override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            4
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            textView.text = s;
         }
     }
 }
