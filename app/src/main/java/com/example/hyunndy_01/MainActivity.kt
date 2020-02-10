a2c1492eb033db577d39a0cf48aa53243e249736
user <yoosa3004@naver.com>
2020-02-10 22:28:11 +0900

    for Studying ListView

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -13,58 +13,37 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
+
+    // 1.
+    // 코틀린의 배열은 arrayOf
+    var data = arrayOf("리스트1", "리스트2", "리스트3")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 2.
+        //컨텍스트 : 어떠한 작업을 위해 필요한것들이 담아져있는 것.
+        // 안드로이드 컨텍스트 화면에 필요한것, OS 등
+        // 컨텍스트, 레이아웃,
+        var adater = ArrayAdapter(this, android.R.layout.simple_list_item_1 , data);
+        listview.adapter = adater;
 
-        button.setOnClickListener { view ->
-            textView.text = editText.text;
+
+        //3.
+        //리스너 : 리스트의 항목을 터치하면 발생하는 리스너
+        var listner = ListListener();
+        listview.setOnItemClickListener(listner);
+
+        listview.setOnItemClickListener { parent, view, position, id ->
+            textView.text = data[position]
         }
-
-        button2.setOnClickListener { view ->
-            editText.setText("지존현지");
-        }
-
-
-       // var listener1 = EnterListener();
-       // editText.setOnEditorActionListener(listener1);
-
-        editText.setOnEditorActionListener { v, actionId, event ->
-            textView.text = editText.text;
-            false
-        }
-
-        var watch = Watcher();
-        editText.addTextChangedListener(watch);
-
-        editText.addTextChangedListener( object : TextWatcher {
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                textView.text = s;
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-
-            }
-        })
     }
 
-    inner class Watcher : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            textView.text = s;
+    inner class ListListener : AdapterView.OnItemClickListener
+    {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            textView.text = data[position];
         }
     }
 }
