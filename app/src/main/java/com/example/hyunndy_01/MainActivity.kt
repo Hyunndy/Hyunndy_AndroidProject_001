e2805bfc189a574ba44428d1dbc2fc0307879156
user <yoosa3004@naver.com>
2020-02-14 17:05:57 +0900

    for Studying Basic Thread

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -13,6 +13,7 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.provider.CalendarContract
 import android.text.Editable
 import android.text.TextWatcher
@@ -30,73 +31,52 @@ import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
-    // 1. 등록한 권한들 중에서 확인이 필요한 권한들만 목록으로 코드로 짜주면 된다.
-    var permission_list = arrayOf(
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.READ_CONTACTS,
-        Manifest.permission.RECEIVE_SMS,
-        Manifest.permission.SEND_SMS
-    )
-
-    // 2. 권한 확인(안드로이드 마쉬멜로우 이상 버전에서부터만 검사한다)
-    fun checkPermission(){
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
-        {
-            return
-        }
-        else
-        {
-            // Kotlin에서의 for문은 배열을 도는 용도.
-            for(permission : String in permission_list)
-            {
-                // 권한 허용했는지 묻는 용도(INT)
-                var chk = checkCallingOrSelfPermission(permission)
-
-                // 만일 거절했었다면,
-                if(chk == PackageManager.PERMISSION_DENIED)
-                {
-                    // 권한 승인 요청
-                    requestPermissions(permission_list, 0)
-
-                    break
-                }
-            }
-        }
-    }
-
-    // 3. 권한 승인 요청 후 다이얼로그 없어졌을 때 어떤 권한들이 승인되었는지 알아보는 함수
-    // permissions : 체크한 권한들의 목록 배열
-    // grantResults : 해당 배열이 승인되었는지 안되었는지 여부
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        var idx = 0;
-
-        // indices를붙으면 배열의 인덱스 번호,
-        // 안붙이면 배열의 값.
-        for(idx in grantResults.indices)
-        {
-            if(grantResults[idx] == PackageManager.PERMISSION_GRANTED)
-            {
-                textView.append("${permission_list[idx]} : 허용함\n")
-            }
-            else
-            {
-                textView.append("${permission_list[idx]} : 거부함\n")
-            }
-        }
-
-
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
+    var isRunning = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        checkPermission()
+        button.setOnClickListener { view ->
+            var now = System.currentTimeMillis()
+            textView.text = "버튼클릭 : ${now}"
+        }
+
+        // 1. Activity에 있는 코드가 굉장히 바쁘면 화면 처리가 안될 수 있다.
+        // ANR 유발 코드.
+        /*
+        while(true)
+        {
+            var now = System.currentTimeMillis()
+            textView2.text = "무한 루프 : ${now}"
+        }
+        */
+
+        // 3. 액티비티가 종료되면 안드로이드os가 굴리는 메인쓰레드만 종료되며,
+        // 내가 만든 쓰레드는 계속 이어진다.
+
+        isRunning = true
+        var thread = ThreadClass1()
+        thread.start()
+    }
+
+    // 2. 쓰레드 클래스 생성
+    inner class ThreadClass1 : Thread()
+    {
+        override fun run() {
+            while(isRunning)
+            {
+                // 100ms만큼 이 부분에서 쉰다는 뜻
+                SystemClock.sleep(100)
+                var now = System.currentTimeMillis()
+                Log.d("test1", "쓰레드 : ${now}")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        isRunning = false
     }
 }
