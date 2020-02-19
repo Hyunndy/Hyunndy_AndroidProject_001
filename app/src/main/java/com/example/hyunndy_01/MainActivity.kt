bde34573d4b7d66b75e8b07d600edbba3d235f84
user <yoosa3004@naver.com>
2020-02-19 23:21:32 +0900

    for Studying SQLite, SQL문말고 클래스를 이용해서 똑같은 기능 구현하기.

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -41,24 +41,46 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener { view ->
 
             // 5. DB 오픈
-            var helper = DBHelper(this)
-            var db = helper.writableDatabase
+           var helper = DBHelper(this)
+           var db = helper.writableDatabase
 
-            // 6. sql 쿼리 작성
-            var sql = "insert into TestTable (textData, intData, floatData, dataData) values (?, ?, ?, ?)"
+           //// 6. sql 쿼리 작성
+           //var sql = "insert into TestTable (textData, intData, floatData, dataData) values (?, ?, ?, ?)"
 
 
-            // 날짜(date)를 패턴으로 지정한 문자열로 뽑아낼 수 있는것
+           //// 날짜(date)를 패턴으로 지정한 문자열로 뽑아낼 수 있는것
+           //var sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+           //ar date = sdf.format(Date())
+
+           //// 7. 쿼리에 들어갈 데이터 세팅
+           //var arg1 = arrayOf("문자열1", "100", "11.11", date)
+           //var arg2 = arrayOf("문자열2", "200", "22.22", date)
+
+           //db.execSQL(sql, arg1)
+           //db.execSQL(sql, arg2)
+
+
+            // 1. 클래스를 이용하는 방법 - INSERT
             var sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-           var date = sdf.format(Date())
+            var date = sdf.format(Date())
 
-            // 7. 쿼리에 들어갈 데이터 세팅
-            var arg1 = arrayOf("문자열1", "100", "11.11", date)
-            var arg2 = arrayOf("문자열2", "200", "22.22", date)
+            // (textData, intData, floatData, dataData) values (?, ?, ?, ?) -> 1:1 배치를 함수로 표현.
+            var cv1 = ContentValues()
+            cv1.put("textData", "문자열1")
+            cv1.put("intData", 100)
+            cv1.put("floatData", 11.11)
+            cv1.put("dataData", date)
 
-            db.execSQL(sql, arg1)
-            db.execSQL(sql, arg2)
+            db.insert("TestTable", null, cv1)
 
+            var cv2 = ContentValues()
+            cv2.put("textData", "문자열2")
+            cv2.put("intData", 200)
+            cv2.put("floatData", 22.22)
+            cv2.put("dataData", date)
+
+
+            db.insert("TestTable", null, cv2)
             db.close()
 
             textView.text = "저장 완료"
@@ -70,10 +92,13 @@ class MainActivity : AppCompatActivity() {
             var helper = DBHelper(this)
             var db = helper.writableDatabase
 
-            var sql = "select * from TestTable"
+            //var sql = "select * from TestTable"
+            //// 커서 클래스타입의 객체는 select해서 가져올 수 있는 객체의 클래스 타입이다.
+            //var c: Cursor = db.rawQuery(sql, null)
 
-            // 커서 클래스타입의 객체는 select해서 가져올 수 있는 객체의 클래스 타입이다.
-            var c: Cursor = db.rawQuery(sql, null)
+            // 2. 클래스로 사용하기 - SELECT
+            // (테이블 이름 , 가져올 컬럼 이름의 문자열 배열, 조건절(값이 들어가는 부분은 ?로 작성. a=? and b=?), 조건절에 세팅될 값의 문자열 배열, 정렬 기준, having절, orderBy절)
+            var c = db.query("TestTable", null, null, null, null, null, null)
 
             textView.text = ""
 
@@ -110,9 +135,17 @@ class MainActivity : AppCompatActivity() {
             var helper = DBHelper(this)
             var db = helper.writableDatabase
 
-            var sql = "update TestTable set textData=? where idx=?"
-            var args = arrayOf("문자열3", "1")
-            db.execSQL(sql, args)
+            //var sql = "update TestTable set textData=? where idx=?"
+            //var args = arrayOf("문자열3", "1")
+            //db.execSQL(sql, args)
+
+            // 3. 클래스로 사용하기 - UPDATE
+            var cv = ContentValues()
+            cv.put("textData", "문자열3")
+            var where = "idx=?"
+            var args = arrayOf("1")
+
+            db.update("TestTable", cv, where, args)
 
             db.close()
 
@@ -124,10 +157,16 @@ class MainActivity : AppCompatActivity() {
             var helper = DBHelper(this)
             var db = helper.writableDatabase
 
-            var sql = "delete from TestTable where idx =?"
+           //var sql = "delete from TestTable where idx =?"
+           //var args = arrayOf("1")
+
+           //db.execSQL(sql, args)
+
+            var where ="idx=?"
             var args = arrayOf("1")
 
-            db.execSQL(sql, args)
+            db.delete("TestTable", where, args)
+
             db.close()
 
             textView.text = "삭제완료"
