a868c35e1c97b0f1a41ae7a16a18358a3cda2c2b
user <yoosa3004@naver.com>
2020-02-19 23:04:46 +0900

    for Studying SQLite, 데이터 뽑아오기&삭제&업데이트

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.*
 import android.content.*
 import android.content.DialogInterface.*
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -62,5 +63,75 @@ class MainActivity : AppCompatActivity() {
 
             textView.text = "저장 완료"
         }
+
+        // 7. 데이터 가져오기
+        button2.setOnClickListener { view ->
+
+            var helper = DBHelper(this)
+            var db = helper.writableDatabase
+
+            var sql = "select * from TestTable"
+
+            // 커서 클래스타입의 객체는 select해서 가져올 수 있는 객체의 클래스 타입이다.
+            var c: Cursor = db.rawQuery(sql, null)
+
+            textView.text = ""
+
+            // 현재에서 다음 row(열)로 계속 넘어간다. 더이상 접근할게 없으면 false.
+            // 각 column(행)의 데이터를 가져오려면 column의 이름을 던져서 해당 컬럼이 몇번째 인덱스인지 받아오고, 그 인덱스가 가진 데이터를 가져와야한다.
+            while(c.moveToNext())
+            {
+
+                var idx_pos = c.getColumnIndex("idx")
+                var textData_pos = c.getColumnIndex("textData")
+                var intData_pos = c.getColumnIndex("intData")
+                var floatData_pos = c.getColumnIndex("floatData")
+                var DateData_pos = c.getColumnIndex("dataData")
+
+                var idx = c.getInt(idx_pos)
+                var textData = c.getString(textData_pos)
+                var intData = c.getInt(intData_pos)
+                var floatData = c.getDouble(floatData_pos)
+                var dateData = c.getString(DateData_pos)
+
+                textView.append("idx : ${idx}\n")
+                textView.append("textData : ${textData}\n")
+                textView.append("intData : ${intData}\n")
+                textView.append("floatData : ${floatData}\n")
+                textView.append("dateData : ${dateData}\n")
+
+            }
+
+            db.close()
+        }
+
+        // 8. 데이터 업데이트
+        button3.setOnClickListener { view ->
+            var helper = DBHelper(this)
+            var db = helper.writableDatabase
+
+            var sql = "update TestTable set textData=? where idx=?"
+            var args = arrayOf("문자열3", "1")
+            db.execSQL(sql, args)
+
+            db.close()
+
+            textView.text = "수정 완료"
+        }
+
+        // 9. 데이터 삭제
+        button4.setOnClickListener { view ->
+            var helper = DBHelper(this)
+            var db = helper.writableDatabase
+
+            var sql = "delete from TestTable where idx =?"
+            var args = arrayOf("1")
+
+            db.execSQL(sql, args)
+            db.close()
+
+            textView.text = "삭제완료"
+        }
+        
     }
 }
