28715192ca652d315db459c9debfdbe7e5e53ee8
user <yoosa3004@naver.com>
2020-02-19 13:38:00 +0900

    for Studying IPC

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -2,11 +2,8 @@ package com.example.hyunndy_01
 
 import android.Manifest
 import android.app.*
-import android.content.Context
-import android.content.DialogInterface
+import android.content.*
 import android.content.DialogInterface.*
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -33,41 +30,72 @@ import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
-    var survice_intent:Intent? = null
+    // 7. IPC 서비스객체를 받아올 변수
+    var ipc_service:ServiceClass1? = null
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1. 서비스 가동
-        button.setOnClickListener {  view ->
-            survice_intent = Intent(this, ServiceClass1::class.java)
-            startService(survice_intent)
+
+        // 3. IPCService를 할 클래스 객체를 Intent에 넣어서 생성
+        var intent = Intent(this, ServiceClass1::class.java)
+
+        // 만일 실행중이 아니라면, 실행시킨다.
+        if(!isServiceRunning("package com.example.hyunndy_01"))
+        {
+            startService(intent)
         }
 
-        // 2. 서비스 종료
-        button2.setOnClickListener { view ->
-            stopService(survice_intent)
-        }
+        // 8.
+        // 서비스에 접속!
+        // bindService()를 안하고 그냥 startService()해버려도 켜지긴하는데 서비스 종료하면 그냥 강제종료되버려서 재접속 시 문제가 생길 수 있으므로 서비스 키고, 서비스에 접속하자.
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE)
 
-        // 3. 인텐트 서비스 가동
-        button3.setOnClickListener { view ->
-            survice_intent = Intent(this, ServiceClass2::class.java)
-            startService(survice_intent)
-            finish()
+        button.setOnClickListener { view ->
+            var value = ipc_service?.getNumber()
+            textView.text = "value : ${value}"
         }
+    }
 
-        // 4. 포그라운드 서비스 가동
-        button4.setOnClickListener { view ->
-            survice_intent = Intent(this, ServiceClass3::class.java)
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+    // 9. 서비스 해제
+    override fun onDestroy() {
+        super.onDestroy()
+
+        unbindService(mConnection)
+    }
+
+
+    // 2.
+    // 서비스를 실행시킨다.
+    // 현재 실행중인 서비스의 목록을 순회하며 매개변수로 들어간 클래스이름과 같은 서비스가 없다면 return false.
+    fun isServiceRunning(name:String):Boolean {
+        var manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+        // manager.getRunningServices(Int.MAX_VALUE) -> 현재 실행중인 서비스들의 정보가 넘어온다.
+        for (service : ActivityManager.RunningServiceInfo in manager.getRunningServices(Int.MAX_VALUE))
+        {
+            if(service.service.className.equals(name))
             {
-                startForegroundService(survice_intent)
+                return true
             }
-            else {
-                startService(survice_intent)
-            }
+        }
+        return false
+    }
+
+    // 7. 커넥션 생성
+    // 서비스 클래스의 OnBind() 메서드가 반환하는 Binder 객체가 이 OnServiceConnected()에 넘어옵니다.
+    private val mConnection = object : ServiceConnection
+    {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+
+            // 서비스 객체를 얻어온다!
+            val binder = service as ServiceClass1.LocalBinder
+            ipc_service = binder.getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            ipc_service = null
         }
     }
 }
--- app/src/main/java/com/example/hyunndy_01/ServiceClass1.kt
+++ app/src/main/java/com/example/hyunndy_01/ServiceClass1.kt
@@ -2,50 +2,68 @@ package com.example.hyunndy_01
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.os.SystemClock
 import android.util.Log
 
 class ServiceClass1 : Service() {
 
+    // 1. IPC 개념을 사용해서 이 value값을 액티비티에서 가져와서 사용해보도록 한다.
+    var value = 0
+    var thread:ThreadClass? = null
+
+    // 6. 바인더 객체 생성
+    var binder : IBinder = LocalBinder()
+    // 외부에서 서비스에 접속하게 되면 자동으로 onBind()가 호출된다.
+    // 이 함수가 리턴하는 IBinder 객체를 액티비티에서 얻어와서 서비스가 사용하는 값을 사용할 수 있다.
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return binder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-
-        // 5. 시작하면 불리는 메소드에 쓰레드 start()처리.
-        var thread = ThreadClass()
-        thread.start()
+        // 1. 서비스에서 가동시킬 쓰레드 가동
+        thread = ThreadClass()
+        thread?.start()
 
         return super.onStartCommand(intent, flags, startId)
     }
 
-
-    // 6. 서비스가 종료되면 불리는 메서드.
     override fun onDestroy() {
         super.onDestroy()
 
         Log.d("test1", "서비스 실행 종료!")
     }
 
-    // 4.
-    // 시작하면 쓰레드에서 작업처리를 하기 위해 스레드 하나 생성.
-    // 인텐트 서비스를 사용하면 이렇게 스레드 클래스를 따로 만들 필요없이 바로 인텐트 서비스의 onHandleIntent()를 사용한다.
     inner class ThreadClass : Thread(){
 
         override fun run() {
-            var idx = 0
-            while(idx <10)
+            while(true)
             {
-                SystemClock.sleep(100)
-                var time = System.currentTimeMillis()
-                Log.d("test1", "Service Running : ${time}")
-
-                idx++
+                SystemClock.sleep(1000)
+                //var time = System.currentTimeMillis()
+                Log.d("test1", "Service Running : ${value}")
+                value++
             }
         }
+    }
 
+    // 4.
+    // IPC 개념을 사용하기 위해 액티비티에서 value값을 얻어올텐데 getNumber()을 사용해서 값을 얻어오려면 이 클래스의 객체를 생성하고 그 객체를 통해서 얻어오는 수 밖에 없다.
+    // 하지만 이 클래스의 객체는 안드로이드OS가 만드는거라 개발자의 입장에서 주소값을 직접적으로 가져올 수는 없다.
+    // 그래서 안드로이드에서는 Binder라는 클래스를 이용해서 액티비티-서비스의 중간단계 역할을 하게 한다.
+    // 따라서 액티비티에서 바인더객체를 통해 서비스에서 사용하는 값을 가져올 수 있다.
+    fun getNumber() : Int{
+        return value
+    }
+
+    // 5. 바인더 생성
+    inner class LocalBinder : Binder()
+    {
+        // 바인더를 통해 서비스 객체의 주소값을 액티비티에게 반환.
+        fun getService() : ServiceClass1{
+            return this@ServiceClass1
+        }
     }
 }
