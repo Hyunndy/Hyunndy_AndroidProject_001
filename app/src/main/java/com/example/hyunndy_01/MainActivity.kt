957990f8f2a4deeef5cbd43a7ad7b7b2e2f1ae55
user <yoosa3004@naver.com>
2020-02-19 22:41:22 +0900

    for Studying SQLite, 쿼리에 데이터 추가

--- app/src/main/java/com/example/hyunndy_01/MainActivity.kt
+++ app/src/main/java/com/example/hyunndy_01/MainActivity.kt
@@ -27,6 +27,7 @@ import org.w3c.dom.Text
 import java.io.*
 import java.lang.Exception
 import java.sql.Types.NULL
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.HashMap
 
@@ -42,8 +43,24 @@ class MainActivity : AppCompatActivity() {
             var helper = DBHelper(this)
             var db = helper.writableDatabase
 
+            // 6. sql 쿼리 작성
+            var sql = "insert into TestTable (textData, intData, floatData, dataData) values (?, ?, ?, ?)"
+
+
+            // 날짜(date)를 패턴으로 지정한 문자열로 뽑아낼 수 있는것
+            var sdf = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+           var date = sdf.format(Date())
+
+            // 7. 쿼리에 들어갈 데이터 세팅
+            var arg1 = arrayOf("문자열1", "100", "11.11", date)
+            var arg2 = arrayOf("문자열2", "200", "22.22", date)
+
+            db.execSQL(sql, arg1)
+            db.execSQL(sql, arg2)
 
             db.close()
+
+            textView.text = "저장 완료"
         }
     }
 }
